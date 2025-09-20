3dc543204ff646bbb149c4de4c7b514475e0063e
sushanksinha <sushanksinha.5252@gmail.com>
2025-09-20 23:48:24 +0530

    add comments for the respective functions

--- ch_3_command_line/input_from_cli.js
+++ ch_3_command_line/input_from_cli.js
@@ -1,13 +1,16 @@
-// const readline = require('readline').createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
+//get input from a readable stream  such as the process.stdin stream
 
-// readline.question('How are you?', (ans)=>{
-//     console.log(`${ans}!`);
-//     readline.close();
-// });
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
+readline.question('How are you?', (ans)=>{
+    console.log(`${ans}!`);
+    readline.close();
+});
+
+//A more abstract solution is provided by the Inquirer.js package
 const {input} = require('@inquirer/prompts');
 
 async function askQuestions() {
